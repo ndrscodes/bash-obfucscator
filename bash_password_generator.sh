07c856414f4447fddbdf8557cc0aed3afbb1bdd9
ndrscodes <98schmidtandreas@gmail.com>
2021-07-13 15:05:39 +0200

    improve readable and compressed code generation

--- bash_password_generator.sh
+++ bash_password_generator.sh
@@ -15,16 +15,21 @@
 # limitations under the License.
 
 shift_size=2
-readable_line_prefix="\t"
-readable_line_suffix="\n"
-non_readable_line_suffix="; "
+readable_line_prefix="\n"
+readable_line_suffix=""
+readable_structure_char="\t"
+non_readable_structure_char=" "
+non_readable_line_suffix=";"
 
 if [[ $1 == "-r" || $1 == "--readable" ]]; then
 	line_suffix=$readable_line_suffix
 	line_prefix=$readable_line_prefix
+        structure_char=$readable_structure_char
 	shift
 else
 	line_suffix=$non_readable_line_suffix
+	structure_char=$non_readable_structure_char
+	line_prefix=" "
 fi
 
 function get_random_name {
@@ -39,11 +44,11 @@ for param in "$@"; do
 	random_func_name=$(get_random_name)
 	random_var_name=$(get_random_name)
 
-	echo -n "function $random_func_name { $line_suffix"
-	echo -n "$line_prefix local $random_var_name=\"\"$line_suffix"
+	echo -n "function $random_func_name {"
+	echo -n "$line_prefix${structure_char}local $random_var_name=\"\"$line_suffix"
 	while IFS= read -n1 c; do
-		echo -n "$line_prefix $random_var_name+=\"\$(printf \"\\\x\$(printf %x \$(($(($(get_keycode "$c")<<$shift_size))>>$shift_size)))\")\"$line_suffix"
+		echo -n "$line_prefix$structure_char$random_var_name+=\"\$(printf \"\\\x\$(printf %x \$(($(($(get_keycode "$c")<<$shift_size))>>$shift_size)))\")\"$line_suffix"
 	done < <(echo -n $param);
-	echo -n "$line_prefix echo \$$random_var_name$line_suffix";
-	echo " }\n"
+	echo -n "$line_prefix${structure_char}echo \$$random_var_name$line_suffix";
+	echo "$line_prefix}\n"
 done
