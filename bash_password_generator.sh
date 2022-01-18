e56b2c8270afcaf51bfc0016b364963b0f239c88
Andreas Schmidt <ndrscodes@gmail.com>
2022-01-18 23:55:33 +0100

    update license

--- bash_password_generator.sh
+++ bash_password_generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021 ndrscodes (Andreas Schmidt)
+# Copyright 2022 ndrscodes (Andreas Schmidt)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
